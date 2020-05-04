077c1c6274b09719cdf3da2d4c887f903a790880
yuri <snicksnk@MacBook-Pro-yuri.local>
2020-05-04 03:15:58 +0300

    Build

---
+++ precache-manifest.81a99295ae3cf561c7edd6bc2e8b12be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14db1e73b23cd05134de1a0c34c02b5",
+    "url": "/react-hooks-d3-barchart/build/index.html"
+  },
+  {
+    "revision": "20d7b5c2011152f115c5",
+    "url": "/react-hooks-d3-barchart/build/static/css/main.e83baf58.chunk.css"
+  },
+  {
+    "revision": "5e73c45a02230c191b95",
+    "url": "/react-hooks-d3-barchart/build/static/js/2.bbcb1d4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-d3-barchart/build/static/js/2.bbcb1d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d7b5c2011152f115c5",
+    "url": "/react-hooks-d3-barchart/build/static/js/main.8f7ccc35.chunk.js"
+  },
+  {
+    "revision": "15069ecbb7e3ae86a570",
+    "url": "/react-hooks-d3-barchart/build/static/js/runtime-main.a7e6ca19.js"
+  }
+]);

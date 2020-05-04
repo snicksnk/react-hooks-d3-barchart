22d445874eb3541dfd93e3f53e0249a87eb2a50c
yuri <snicksnk@MacBook-Pro-yuri.local>
2020-05-04 03:25:40 +0300

    Rebuild

---
+++ precache-manifest.e9f50b6795ee9d69f01d8015773c716f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176994509ff45631e2b99234657fc600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f86506cdeab00d00515",
+    "url": "/static/css/main.e83baf58.chunk.css"
+  },
+  {
+    "revision": "5e73c45a02230c191b95",
+    "url": "/static/js/2.bbcb1d4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bbcb1d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f86506cdeab00d00515",
+    "url": "/static/js/main.45b77582.chunk.js"
+  },
+  {
+    "revision": "000a2f217d651fa5012b",
+    "url": "/static/js/runtime-main.a918fc6e.js"
+  }
+]);
